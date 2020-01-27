bb88c388cb3a91b2567171a81189fb60c40203e3
miguelgarcia210 <garcia.a.miguel.210@gmail.com>
2020-01-27 10:39:09 -0600

    3 done

--- src/main/java/com/codeup/springblog/controllers/MathController.java
+++ src/main/java/com/codeup/springblog/controllers/MathController.java
@@ -13,4 +13,14 @@ public class MathController {
     public String addTwoNumbers(@PathVariable int n1, @PathVariable int n2) {
         return "The sum of the two is " + (n1 + n2);
     }
+
+//    /subtract/3/from/10
+//    /multiply/4/and/5
+//    /divide/6/by/3
+
+    @GetMapping("/subtract/{n1}/from/{n2}")
+    @ResponseBody
+    public String subtractTwoNumbers(@PathVariable int n1, @PathVariable int n2) {
+        return "The subtraction amounted to " + (n2 - n1);
+    }
 }
